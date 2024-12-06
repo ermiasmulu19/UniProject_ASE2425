6a4cbaa2a3037ea8c242d5a9097c2f2a66147f6e
ermias19 <ermiasmulu19@gmail.com>
2024-12-06 16:44:23 +0100

    security for gacha crud oprations are added.

--- gacha_game/urls.py
+++ gacha_game/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from api import views
 from api.views import register_api,login_api,user_delete_api,spin_duck_api,roll_gacha_api,user_logout, profile,modify_user
 from api.views import my_gacha_collection, gacha_info, system_gacha_collection, system_gacha_info, buy_currency,transaction_history, secure_place_bid_api
+from api.views import create_gacha,update_gacha,delete_gacha,admin_view_all_gachas
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,7 +15,7 @@ router = DefaultRouter()
 urlpatterns = [
     path('home/', views.home_api, name='home'),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)), 
+    path('api/', include(router.urls)), 
     path('register/', register_api, name='register'),
     path('login/', login_api, name='login'),
     path('logout/', user_logout, name='logout'),
@@ -28,9 +29,14 @@ urlpatterns = [
     path('gacha-info/<int:gacha_id>/', gacha_info, name='gacha_info'),
     path('system-collection/', system_gacha_collection, name='system_gacha_collection'),
     path('system-gacha-info/<int:gacha_id>/', system_gacha_info, name='system_gacha_info'),
-    path('buy-currency/', buy_currency, name='buy_currency'),
+    path('buy_currency/', buy_currency, name='buy_currency'),
     path('transactions/', transaction_history, name='transaction_history'),
     path('secure-bid/<int:auction_id>/', secure_place_bid_api, name='secure_place_bid'),
+    path('manag/duck/create/', create_gacha, name='create_gacha'),
+    path('manag/duck/<int:gacha_id>/update/', update_gacha, name='update_gacha'),
+    path('manag/duck/<int:gacha_id>/delete/', delete_gacha, name='delete_gacha'),
+    path('manag/duck/all/', admin_view_all_gachas, name='admin_view_all_gachas'),
+    # path('admin/logout/', admin_logout, name='admin_logout'),
 ]
 
 
